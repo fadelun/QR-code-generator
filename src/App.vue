04c4ab9e98fc6adc0d6e7d8d0dc18b06506554dd
Muhammad Fadll <fadeljourney@gmail.com>
2023-04-28 08:40:03 +0700

    add new project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <QRCode />
+</template>
+
+<script setup>
+import QRCode from "./components/QRCode.vue";
+</script>
---
+++ src/components/QRCode.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <h1>QR CODE GENERATOR</h1>
+    </header>
+    <form>
+      <p>Paste a url to create QR code</p>
+      <input type="text" name="input-link" placeholder="Enter your url" />
+      <button
+        @click.prevent="() => (showQRCode = !showQRCode)"
+        class="btn-generate"
+      >
+        Generate QR code
+      </button>
+    </form>
+    <div :class="`qr-code ${showQRCode ? 'active' : ''}`">
+      <img src="./../assets/QR_code.png" alt="QR code" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const showQRCode = ref(false);
+</script>
