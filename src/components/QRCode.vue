07c044eea6a6b2585d2f82023fcab0f2f61ffa4b
Muhammad Fadll <fadeljourney@gmail.com>
2023-04-29 14:08:48 +0700

    update generate code

--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -5,16 +5,19 @@
     </header>
     <form>
       <p>Paste a url to create QR code</p>
-      <input type="text" name="input-link" placeholder="Enter your url" />
-      <button
-        @click.prevent="() => (showQRCode = !showQRCode)"
-        class="btn-generate"
-      >
+      <input
+        type="text"
+        name="input-link"
+        placeholder="Enter your url"
+        v-model="inputURL"
+        v-on:keyup="if (!inputURL) showQRCode = false;"
+      />
+      <button @click.prevent="handleQRCode" class="btn-generate">
         Generate QR code
       </button>
     </form>
     <div :class="`qr-code ${showQRCode ? 'active' : ''}`">
-      <img src="./../assets/QR_code.png" alt="QR code" />
+      <img src="result" alt="QR code" />
     </div>
   </div>
 </template>
@@ -23,4 +26,17 @@
 import { ref } from "vue";
 
 const showQRCode = ref(false);
+const inputURL = ref("");
+
+const handleQRCode = () => {
+  const wrapper = document.querySelector(".wrapper");
+  const image = wrapper.querySelector("img");
+
+  if (inputURL.value) {
+    image.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inputURL.value}`;
+
+    return (showQRCode.value = true);
+  }
+};
+// API : https://goqr.me/api/
 </script>
