24d62750d9b7879fc7ca416c033d9de6876048fc
Muhammad Fadll <fadeljourney@gmail.com>
2023-05-01 11:40:33 +0700

    add color and Bgcolor options

--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -12,12 +12,34 @@
         v-model="inputURL"
         v-on:keyup="if (!inputURL) showQRCode = false;"
       />
+      <div class="option">
+        <div class="input-color flex items-center w-2/4">
+          <label for="color">color</label>
+          <input
+            type="color"
+            name="color"
+            id="color"
+            v-model="inputColor"
+            @change="check"
+          />
+        </div>
+        <div class="input-bgcolor">
+          <label for="bgcolor">bgcolor</label>
+          <input
+            type="color"
+            name="bgcolor"
+            id="bgcolor"
+            v-model="inputBgcolor"
+            @change="check"
+          />
+        </div>
+      </div>
       <button @click.prevent="handleQRCode" class="btn-generate">
         Generate QR code
       </button>
     </form>
     <div :class="`qr-code ${showQRCode ? 'active' : ''}`">
-      <img src="result" alt="QR code" />
+      <img src="result" alt="QR code" :class="{ hidden: !showQRCode }" />
     </div>
   </div>
 </template>
@@ -27,13 +49,24 @@ import { ref } from "vue";
 
 const showQRCode = ref(false);
 const inputURL = ref("");
+const inputColor = ref("");
+const inputBgcolor = ref("");
+
+const check = (e) => {
+  return e.target.value;
+};
 
 const handleQRCode = () => {
   const wrapper = document.querySelector(".wrapper");
   const image = wrapper.querySelector("img");
 
   if (inputURL.value) {
-    image.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inputURL.value}`;
+    image.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${
+      inputURL.value
+    }&color=${inputColor.value.substr(1)}&bgcolor=${inputBgcolor.value.substr(
+      1,
+    )}
+    `;
 
     return (showQRCode.value = true);
   }
